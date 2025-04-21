e3af5e44821cbf742d712e93f1283283979697bc
Joseph Massie <josephmmassie@gmail.com>
2025-04-21 14:25:35 -0700

    Implemented Sentry for real time error reporting

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+    dsn: 'https://20123a0925f7860ceffb2cc84df91e9a@o4509193296674816.ingest.us.sentry.io/4509193297592320',
+
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
+
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+});
